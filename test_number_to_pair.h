f91b60ce641a20a0111a12114e53f0e55d387b47
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-07-19 12:49:19 +0530

    test_number_to_pair.h

---
+++ test_number_to_pair.h
@@ -0,0 +1,8 @@
+#ifndef TEST_NUMBER_TO_PAIR_H
+#define TEST_NUMBER_TO_PAIR_H
+
+#include "color_codes.h"
+
+void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor);
+
+#endif
