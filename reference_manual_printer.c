136e90ea6c10d169353f0947341252fee50b1975
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-07-22 11:51:41 +0530

    Update reference_manual_generator.c

--- reference_manual_printer.c
+++ reference_manual_printer.c
@@ -2,7 +2,7 @@
 #include "colorpair_to_string_converter.h"
 #include "color_from_pair_number_finder.h"
 
-void print_reference_manual() {
+void generate_reference_manual() {
     char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
     printf("Color Code Reference Manual\n");
     printf("----------------------------\n");
