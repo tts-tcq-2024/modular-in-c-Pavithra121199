027e73c63131f61c8d4318aa44b8d0a1c0e6f5eb
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-07-22 08:39:49 +0530

    colorpair_to_string_converter.c

--- colorpair_to_string_converter.c
+++ colorpair_to_string_converter.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "color_pair_to_string.h"
+#include "colorpair_to_string_converter.h"
 #include "color_codes.h"
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
