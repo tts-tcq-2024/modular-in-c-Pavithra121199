b900d3c38c8100e1b3c013adf479506af93e9f59
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-07-22 13:20:52 +0530

    Update reference_manual_generator.c

--- reference_manual_generator.c
+++ reference_manual_generator.c
@@ -2,13 +2,20 @@
 #include "colorpair_to_string_converter.h"
 #include "color_from_pair_number_finder.h"
 
-void generate_reference_manual() {
-    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-    printf("Color Code Reference Manual\n");
-    printf("----------------------------\n");
+#define MAX_BUFFER_SIZE 1024
+#deine MAX_LINE_SIZE 20
+const char* generate_reference_manual() {
+    static char reference_manual[MAX_BUFFER_SIZE];
+    static char line[MAX_LINE_SIZE];
+    reference_manual[0] = '\0'; // Initialize the string
+    strcat(reference_manual, "Color Code Reference Manual\n");
+    strcat(reference_manual, "----------------------------\n");
     for (int i = 1; i <= numberOfMajorColors * numberOfMinorColors; ++i) {
+        char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
         ColorPair colorPair = GetColorFromPairNumber(i);
         ColorPairToString(&colorPair, colorPairNames);
-        printf("%d: %s\n", i, colorPairNames);
-    }
+        snprintf(line, sizeof(line), "%d: %s\n", i, colorPairNames);
+        strcat(reference_manual, line);
+        }
+    return reference_manual;
 }
